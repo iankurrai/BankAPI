1666fb727d4dcd92c87fc17cc7c7e87adc25f81e
iankurrai <iankurrai@gmail.com>
2021-04-22 22:33:24 +0530

    Initial commit

---
+++ Models/ifsccode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankAPI.Models
+{
+    public class ifsccode
+    {
+        public string Branch_Name { get; set; }
+        public string IFSC_code { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BankAPI
+ Web API 
